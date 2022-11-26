6dbf2405896de73b8c7f1a512b8ede8bd718e6b8
pejman <pejman.hkh@gmail.com>
2022-11-26 16:42:48 +0330

    bind install

--- bind/install.sh
+++ bind/install.sh
@@ -1,5 +1,7 @@
-wget ftp://ftp.isc.org/isc/bind9/9.18.0/bind-9.18.0.tar.xz
-tar -xf bind-9.18.0.tar.xz
+source "../fns.sh"
+getUrl ftp://ftp.isc.org/isc/bind9/9.18.0/bind-9.18.0.tar.xz
+extractFile bind-9.18.0.tar.xz
+cd bind-9.18.0
 ./configure --prefix=$(pwd)
 make -j 4
 make install
