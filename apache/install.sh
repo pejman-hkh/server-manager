be42deabf7471362b3421b1d6c0ca595d4f64407
pejman <pejman.hkh@gmail.com>
2022-11-27 17:41:15 +0330

    e

--- apache/install.sh
+++ apache/install.sh
@@ -1,11 +1,6 @@
-if [ ! -f "httpd-2.4.54.tar.gz" ]; then
-	wget https://dlcdn.apache.org/httpd/httpd-2.4.54.tar.gz
-fi
-
-if [ ! -d "httpd-2.4.54" ]; then
-	tar -xvzf httpd-2.4.54.tar.gz
-fi
-
+source "../fns.sh"
+getUrl https://dlcdn.apache.org/httpd/httpd-2.4.54.tar.gz
+extractFile httpd-2.4.54.tar.gz
 sudo apt install libapr1 libapr1-dev libaprutil1-dev
 ./configure --prefix=$(pwd)
 make -j 4
