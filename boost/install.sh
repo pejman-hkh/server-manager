27289ef724b10e6839269d76339f1f2c730eb1fe
pejman <pejman.hkh@gmail.com>
2022-11-27 15:42:05 +0330

    e

--- boost/install.sh
+++ boost/install.sh
@@ -1,1 +1,4 @@
-wget https://boostorg.jfrog.io/artifactory/main/release/1.77.0/source/boost_1_77_0.tar.bz2
+source "../fns.sh"
+getUrl https://boostorg.jfrog.io/artifactory/main/release/1.77.0/source/boost_1_77_0.tar.bz2
+extractFile boost_1_77_0.tar.bz2
+cd boost_1_77_0
